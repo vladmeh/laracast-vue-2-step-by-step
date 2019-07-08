ff26ece33ffbe2ad2027538541347f23e0d163c7
vladmeh <vladmeh@gmail.com>
2019-07-09 01:27:39 +0300

    Episode_12.Component Communication Example #1: Custom Events.

--- main.js
+++ main.js
@@ -1,68 +1,24 @@
-Vue.component('tabs', {
-    template: `
-         <div>
-             <div class="tabs">
-                <ul>
-                    <li v-for="tab in tabs" :class="{ 'is-active' : tab.isActive }">
-                        <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
-                    </li>
-                </ul>
-             </div>
-            
-             <div class="tabs-details">
-                <slot></slot>
-             </div>
-         </div>
-        `,
-
-   data() {
-        return { tabs: [] };
-   },
-
-    created() {
-        this.tabs = this.$children;
-    },
+Vue.component('coupon', {
+    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
 
     methods: {
-        selectTab(selectedTab) {
-            this.tabs.forEach(tab => {
-                tab.isActive = (tab.name === selectedTab.name);
-            });
+        onCouponApplied(){
+            this.$emit('applied');
         }
     }
 });
 
-Vue.component('tab', {
-    template: `
-         <div v-show="isActive"><slot></slot></div>
-        `,
-
-    props: {
-        name: {required: true },
-        selected: { default: false }
-    },
-
-    data() {
-        return {
-            isActive: false
-        }
-    },
-
-    computed: {
-        href() {
-            return '#' + this.name.toLowerCase().replace(/ /g, '-');
-        }
-    },
-
-    mounted() {
-        this.isActive = this.selected;
-    }
-});
 
 new Vue({
     el: '#root',
 
     data: {
-        showModal: false
+        couponApplied: false
+    },
+
+    methods: {
+        onCouponApplied(){
+            this.couponApplied = true;
+        }
     }
 });
