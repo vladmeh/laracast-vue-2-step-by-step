105fd5dd575d40c169a0bde0d70004f6ac0f6c0a
vladmeh <vladmeh@gmail.com>
2019-07-10 23:42:50 +0300

    Episode_17.Hot Module Replacement.

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -2,15 +2,21 @@
     <div id="app">
         <message>Hello There</message>
         <message>Hello There Again</message>
+        <counter></counter>
     </div>
 </template>
 
 <script>
     import Message from './components/Message.vue';
+    import Counter from './components/Counter.vue'
 
     export default {
         name: 'app',
-        components: { Message },
+        components: {
+            Message,
+            Counter
+        },
+
         data () {
             return {
 
---
+++ my-app/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Count: {{ count }}</h2>
+        <p>
+            <button @click="count += 1">Increment</button>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Counter",
+        data() {
+            return { count: 0 }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
