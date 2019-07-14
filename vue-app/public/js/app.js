d128f1576d778c0609be2464ccab2c124520a4d0
vladmeh <vladmeh@gmail.com>
2019-07-14 23:52:29 +0300

    Episode_20. Object-Oriented Forms: Part 2.

--- vue-app/public/js/app.js
+++ vue-app/public/js/app.js
@@ -22,7 +22,57 @@ class Errors {
     }
     
     clear(field) {
-        delete this.errors[field];
+        if (field){
+            delete this.errors[field];
+            return;
+        }
+
+        this.errors = {};
+    }
+}
+
+class Form {
+    constructor(data) {
+        this.originalData = data;
+
+        for (let field in data){
+            this[field] = data[field];
+        }
+
+        this.errors = new Errors();
+    }
+
+    data() {
+        let data = Object.assign({}, this);
+
+        delete data.originalData;
+        delete data.errors;
+
+        return data;
+    }
+
+    reset() {
+        for (let field in this.originalData){
+            this[field] = '';
+        }
+    }
+
+    submit(requestType, url) {
+        axios[requestType](url, this.data())
+            .then(this.onSuccess.bind(this))
+            .catch(this.onFail.bind(this))
+        ;
+    }
+
+    onSuccess(response) {
+        alert(response.data.message)
+
+        this.errors.clear();
+        this.reset();
+    }
+
+    onFail(error) {
+        this.errors.record(error.response.data.errors)
     }
 }
 
@@ -30,24 +80,15 @@ new Vue({
     el: '#app',
     
     data: {
-        name: '',
-        description: '',
-        errors: new Errors()
+        form: new Form({
+            name: '',
+            description: ''
+        }),
     },
     
     methods: {
         onSubmit() {
-            axios.post('/projects', this.$data)
-              .then(this.onSuccess)
-              .catch(error => this.errors.record(error.response.data.errors))
-            ;
-        },
-        onSuccess(response) {
-            alert(response.data.message);
-            // this.name = '';
-            // this.description = '';
-            
-            form.reset(); // Cliffhanger duh duh duh
+            this.form.submit('post', '/projects');
         }
     }
 });
