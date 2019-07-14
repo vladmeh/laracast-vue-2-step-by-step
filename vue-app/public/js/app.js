05a639f9b6f0891b0e37ac200b9aae4e3ffa2d60
vladmeh <vladmeh@gmail.com>
2019-07-15 00:44:49 +0300

    Episode_21. Object-Oriented Forms: Part 3

--- vue-app/public/js/app.js
+++ vue-app/public/js/app.js
@@ -43,10 +43,11 @@ class Form {
     }
 
     data() {
-        let data = Object.assign({}, this);
+        let data = {};
 
-        delete data.originalData;
-        delete data.errors;
+        for (let property in this.originalData) {
+            data[property] = this[property];
+        }
 
         return data;
     }
@@ -55,24 +56,44 @@ class Form {
         for (let field in this.originalData){
             this[field] = '';
         }
+
+        this.errors.clear();
+    }
+
+    post(url) {
+        return this.submit('post', url);
+    }
+
+    delete(url) {
+        return this.submit('delete', url);
     }
 
     submit(requestType, url) {
-        axios[requestType](url, this.data())
-            .then(this.onSuccess.bind(this))
-            .catch(this.onFail.bind(this))
-        ;
+
+        return new Promise((resolve, reject) => {
+            axios[requestType](url, this.data())
+                .then(response => {
+                    this.onSuccess(response.data);
+
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    this.onFail(error.response.data.errors);
+
+                    reject(error.response.data.errors)
+                })
+        });
+
+
     }
 
-    onSuccess(response) {
-        alert(response.data.message)
-
-        this.errors.clear();
+    onSuccess(data) {
+        alert(data.message);
         this.reset();
     }
 
-    onFail(error) {
-        this.errors.record(error.response.data.errors)
+    onFail(errors) {
+        this.errors.record(errors)
     }
 }
 
@@ -88,7 +109,7 @@ new Vue({
     
     methods: {
         onSubmit() {
-            this.form.submit('post', '/projects');
+            this.form.post('/projects');
         }
     }
 });
