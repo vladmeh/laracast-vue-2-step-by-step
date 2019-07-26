462549a4af84410eec057fbbd7ca9b8116e47c7e
vladmeh <vladmeh@gmail.com>
2019-07-27 02:01:23 +0300

    Episode_30. Testing Vue: Part 1

--- vue-testing/src/Notification.js
+++ vue-testing/src/Notification.js
@@ -1,5 +1,17 @@
 export default {
-    template: `<div><h1>{{ message }}</h1></div>`,
+    template: `<div><h1>{{ notification }}</h1></div>`,
 
     props: ['message'],
+
+    computed: {
+        notification() {
+            return this.message.toUpperCase();
+        }
+    },
+
+    filters: {
+        capitalize(message) {
+            return message.toUpperCase();
+        }
+    }
 };
--- vue-testing/test/notification.js
+++ vue-testing/test/notification.js
@@ -2,12 +2,20 @@ import Vue from 'vue/dist/vue.js';
 import test from 'ava';
 import Notification from '../src/Notification';
 
-test('that it renders a notification', t => {
+let vm;
+
+test.beforeEach(t => {
     let N = Vue.extend(Notification);
 
-    let vm = new N({ propsData: {
+    vm = new N({ propsData: {
             message: 'Foobar'
         }}).$mount();
+});
 
-    t.is(vm.$el.textContent, 'Foobar');
+test('that it renders a notification', t => {
+    t.is(vm.$el.textContent, 'FOOBAR');
+});
+
+test('that it computed the notification', t => {
+    t.is(vm.notification, 'FOOBAR');
 });
