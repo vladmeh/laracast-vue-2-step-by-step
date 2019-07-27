64048f3c6d7a6d451c248d5ddfacf2529322f2b9
vladmeh <vladmeh@gmail.com>
2019-07-28 00:38:59 +0300

    vue-app fix episodes

--- vue-app/resources/js/app.js
+++ vue-app/resources/js/app.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+import ExampleComponent from './components/ExampleComponent';
+import Notification from './components/Notification';
 import Coupon from './components/Coupon';
 import MenuList from './components/MenuList';
 
@@ -11,7 +13,36 @@ new Vue({
     },
 
     components: {
+        ExampleComponent,
+        Notification,
         Coupon,
         MenuList
     }
 });
+
+if (document.getElementById('one') && document.getElementById('two')){
+    let store = {
+        user: {
+            name: 'John Doe'
+        }
+    };
+
+    new Vue({
+        el: '#one',
+
+        data: {
+            foo: 'bar',
+            shared: store
+        }
+    });
+
+    new Vue({
+        el: '#two',
+
+        data: {
+            foo: 'other',
+            shared: store
+        }
+    });
+}
+
